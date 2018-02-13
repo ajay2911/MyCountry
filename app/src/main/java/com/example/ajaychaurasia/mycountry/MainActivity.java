f6d1f9a9b05271a6058a6778af500914d39c2a77
ajay2911 <hardydude17@gmail.com>
2018-02-13 14:50:07 +0530

    Adding Pull to Refresh Functionality

--- app/src/main/java/com/example/ajaychaurasia/mycountry/MainActivity.java
+++ app/src/main/java/com/example/ajaychaurasia/mycountry/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.ajaychaurasia.mycountry;
 
+import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
@@ -27,6 +28,9 @@ public class MainActivity extends AppCompatActivity {
     @BindView(R.id.recycler_list)
     RecyclerView recyclerList;
 
+    @BindView(R.id.swiperefresh)
+    SwipeRefreshLayout swipeRefreshLayout;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -34,8 +38,21 @@ public class MainActivity extends AppCompatActivity {
         ButterKnife.bind(this);
 
         recyclerList.setLayoutManager(new LinearLayoutManager(this));
+        swipeRefreshLayout.setOnRefreshListener(
+                new SwipeRefreshLayout.OnRefreshListener() {
+                    @Override
+                    public void onRefresh() {
+                        updateData();
+                    }
+                }
+        );
 
         fetchListData();
+    }
+
+    private void updateData(){
+        fetchListData();
+        swipeRefreshLayout.setRefreshing(false);
     }
 
     /*
