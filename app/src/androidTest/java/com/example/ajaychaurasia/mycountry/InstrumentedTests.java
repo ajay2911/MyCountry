f9f60d7efd2bc27c1b91f9cb07e787c6cab2ff1d
ajay2911 <hardydude17@gmail.com>
2018-02-15 22:26:44 +0530

    Renaming test script files and adding Roboelectric for Unit Testing

--- app/src/androidTest/java/com/example/ajaychaurasia/mycountry/InstrumentedTests.java
+++ app/src/androidTest/java/com/example/ajaychaurasia/mycountry/InstrumentedTests.java
@@ -23,7 +23,7 @@ import static org.junit.Assert.assertEquals;
  * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
  */
 @RunWith(AndroidJUnit4.class)
-public class ExampleInstrumentedTest {
+public class InstrumentedTests {
     @Test
     public void useAppContext() throws Exception {
         // Context of the app under test.
--- app/src/test/java/com/example/ajaychaurasia/mycountry/UnitTests.java
+++ app/src/test/java/com/example/ajaychaurasia/mycountry/UnitTests.java
@@ -1,24 +1,17 @@
 package com.example.ajaychaurasia.mycountry;
 
-import com.example.ajaychaurasia.mycountry.pojo.JSONResponseData;
-
 import org.junit.Test;
-import org.mockito.Mock;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 
 /**
  * Example local unit test, which will execute on the development machine (host).
  *
  * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
  */
-public class ExampleUnitTest {
+public class UnitTests {
     @Test
     public void addition_isCorrect() throws Exception {
         assertEquals(4, 2 + 2);
     }
-
-    @Mock
-    JSONResponseData jsonResponseData;
-
 }
